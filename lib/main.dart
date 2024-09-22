f8f5f86a8dcf75cd24adeec95d71a1427719a86f
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-09-22 08:34:30 +0300

    create the project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitsHub());
+}
+
+class FruitsHub extends StatelessWidget {
+  const FruitsHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
